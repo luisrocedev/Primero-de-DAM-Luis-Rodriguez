49a4646eecf20c314e1840bec017b4a6872d6604
luisrocedev <luisilustraciones@gmail.com>
2024-10-17 14:22:58 +0200

    1

---
+++ Entornos de desarrollo/001-Desarrollo de sofware/002-Codigo fuente, codigo objeto y codigo ejecutable; tecnologias de virtualizacion/Multiparadigma python/objetos.py
@@ -0,0 +1,7 @@
+class Gato:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+gato = Gato("Micifu", 2)
+print(f"Hola, mi nombre es {gato.nombre} y tengo {gato.edad} años.")
