789e704872f46696222c7ec54a31075c3b82c972
luisrocedev <luisilustraciones@gmail.com>
2024-10-20 23:41:47 +0200

    1

---
+++ Programacion/005-Lectura y escritura de informacion/007-interfaces graficas/007-etiqueta.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+ventana = tk.Tk()
+ventana.geometry("500x500+200+200")
+ventana.title("El programa de Luis Rodriguez")
+
+tk.Label(ventana,text="Hola mundo este es mi primer programa")
+
+ventana.mainloop()
---
+++ Programacion/005-Lectura y escritura de informacion/007-interfaces graficas/008-empaquetar.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+ventana = tk.Tk()
+ventana.geometry("500x500+200+200")
+ventana.title("El programa de Luis Rodriguez")
+
+tk.Label(ventana,text="Hola mundo este es mi primer programa").pack(padx=10,pady=10)
+ventana.mainloop()
