ddda6d1a57ad6cc358d882e144f5dc7629756500
luisrocedev <luisilustraciones@gmail.com>
2024-10-21 00:26:16 +0200

    1

---
+++ Entornos de desarrollo/004-Optimizacion y documentacion/001-Refactorizacion/006-divido en archivos/principal.py
@@ -0,0 +1,21 @@
+from funciones import*
+
+bienvenida()
+
+while True:
+    
+    muestraMenu()
+
+    entrada = input("Selecciona una opcion:")
+
+    if entrada == "1":
+        insertar()
+
+    elif entrada == "2":
+        listar()
+
+    elif entrada == "3":
+        actualizar()
+
+    elif entrada == "4":
+        eliminar()
