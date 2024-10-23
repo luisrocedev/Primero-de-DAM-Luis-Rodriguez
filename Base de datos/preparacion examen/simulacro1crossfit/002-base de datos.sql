33a270c4e1b9acccccca14b7b460175da23eb157
luisrocedev <luisilustraciones@gmail.com>
2024-10-24 00:12:13 +0200

    1

---
+++ Base de datos/preparacion examen/simulacro1crossfit/002-base de datos.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `tabla clientes`.`tabla clientes`
+    (`ID_Cliente` INT(255) NOT NULL AUTO_INCREMENT ,
+    `Nombre` VARCHAR(255) NOT NULL , `Correo` VARCHAR(255) NOT NULL , `Teléfono` INT(255) NOT NULL 
+    , `Fecha_Registro` DATE NOT NULL ,
+    `ID_Membresía` INT(255) NOT NULL ,
+        PRIMARY KEY (`ID_Cliente`))
+ENGINE = InnoDB;
