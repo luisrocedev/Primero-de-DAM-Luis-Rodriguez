5f166126e63130679050186cd8d1c69aa8187f38
luisrocedev <luisilustraciones@gmail.com>
2024-11-17 20:19:31 +0100

    1

--- Entornos de desarrollo/001-Desarrollo de sofware/006-Proceso de obtencion de codigo ejecutable a partir del codigo fuente/cpp/006-encadenamiento.cpp
+++ Entornos de desarrollo/001-Desarrollo de sofware/006-Proceso de obtencion de codigo ejecutable a partir del codigo fuente/cpp/006-encadenamiento.cpp
@@ -1,9 +1,9 @@
-#include <iostream>                                                                                       // Incluye una libreria de fuljo (stream) de entrada (input) y salida (out)
+#include <iostream> // Incluye una libreria de fuljo (stream) de entrada (input) y salida (out)
 
-int main() {                                                                                              // Creo una funcion, pero es el punto de entrada de la ejecuccion
+int main()
+{ // Creo una funcion, pero es el punto de entrada de la ejecuccion
     int edad = 46.00;
-    std::string nombre = "Luis Rodriguez";                                                                // De la libreria std, importo el tipo de dato string, y creo una variable
-    std::cout << "Hola, mundo desde C++ y me llamo " << nombre << " Y estoy muy contendo de aprender C++";  // De la libreria estanda (std) quiero usar el comando C-exe, y saco una
-    return 0;                                                                                             // Cumplo la promesa de devolver un entero
+    std::string nombre = "Luis Rodriguez";                                                               // De la libreria std, importo el tipo de dato string, y creo una variable
+    std::cout << "Hola mundo desde C++, me llamo " << nombre << " Y estoy muy contendo de aprender C++"; // De la libreria estanda (std) quiero usar el comando C-exe, y saco una
+    return 0;                                                                                            // Cumplo la promesa de devolver un entero
 }
-  
