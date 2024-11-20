56ff093cbae38e6b06749875a20804f040baa3cd
luisrocedev <luisilustraciones@gmail.com>
2024-11-20 17:50:21 +0100

    1

---
+++ Base de datos/Examen base de datos/primeravista.sql
@@ -0,0 +1,1 @@
+SELECT * FROM `cookies` WHERE 1;
---
+++ Base de datos/Examen base de datos/segundavista.sql
@@ -0,0 +1,8 @@
+SELECT
+Identificador AS "ID de cookies",
+titulo AS "Titulo de cookies",
+Subtitulo AS "Subtitulo del cookie"
+FROM cookies
+ORDER BY
+Subtitulo DESC
+;
