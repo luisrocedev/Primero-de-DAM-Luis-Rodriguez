15d57f94618b6b6850c737843019dc18ad7179c2
luisrocedev <luisilustraciones@gmail.com>
2024-11-30 19:40:57 +0100

    1

--- Lenguaje de desarrollo y marcas/003-Manipulacion de documentos web/Proyectos en clase/004-proyecto Apple/014-mas modular/index.php
+++ Lenguaje de desarrollo y marcas/003-Manipulacion de documentos web/Proyectos en clase/004-proyecto Apple/014-mas modular/index.php
@@ -1,14 +1,15 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
-    <head>
-        <title>Apple</title>
-        <link rel="stylesheet" href="estilo/general.css">
-        <link rel="stylesheet" href="estilo/cabecera.css">
-        <link rel="stylesheet" href="estilo/oferta.css">
-    </head>
-    <body>
-        <?php include "modulos/cabecera/cabecera.php"; ?>
-        <?php include "modulos/principal/principal.php"; ?>
-        <?php include "modulos/pidepagina/piedepagina.php"; ?>
-    </body>
+	<head>
+		<title>Apple</title>
+		<link rel="Stylesheet" href="estilo/general.css">
+		<link rel="Stylesheet" href="estilo/cabecera.css">
+		<link rel="Stylesheet" href="estilo/oferta.css">
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<?php include "modulos/cabecera/cabecera.php"; ?>
+		<?php include "modulos/principal/principal.php"; ?>
+		<?php include "modulos/piedepagina/piedepagina.php"; ?>
+	</body>
 </html>
