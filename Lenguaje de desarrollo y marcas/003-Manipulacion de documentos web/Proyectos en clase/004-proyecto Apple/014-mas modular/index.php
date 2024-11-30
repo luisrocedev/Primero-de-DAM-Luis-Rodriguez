6ded3dd7c7eaf65de30790a70af621445ee0200e
Luis Rodriguez <luisrocedev@gmail.com>
2024-11-30 18:12:22 +0100

    1

---
+++ Lenguaje de desarrollo y marcas/003-Manipulacion de documentos web/Proyectos en clase/004-proyecto Apple/014-mas modular/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Apple</title>
+        <link rel="stylesheet" href="estilo/general.css">
+        <link rel="stylesheet" href="estilo/cabecera.css">
+        <link rel="stylesheet" href="estilo/oferta.css">
+    </head>
+    <body>
+        <?php include "modulos/cabecera/cabecera.php"; ?>
+        <?php include "modulos/principal/principal.php"; ?>
+        <?php include "modulos/pidepagina/piedepagina.php"; ?>
+    </body>
+</html>
