7ba0d34b21e6fd4700036d8faf4e8efb8b5f68ef
luisrocedev <luisilustraciones@gmail.com>
2024-11-30 21:56:03 +0100

    1

---
+++ Lenguaje de desarrollo y marcas/003-Manipulacion de documentos web/Proyectos en clase/004-proyecto Apple/017-oferta/modulos/cabecera/cabecera.php
@@ -0,0 +1,17 @@
+
+<header>
+	<div class="contenedor">
+		<img src="img/logo.png">
+		<nav>
+			<ul>
+				
+			</ul>
+		</nav>
+	</div>
+</header>
+<style>
+	<?php include "cabecera.css"?>
+</style>
+<script>
+	<?php include "cabecera.js"?>
+</script>
