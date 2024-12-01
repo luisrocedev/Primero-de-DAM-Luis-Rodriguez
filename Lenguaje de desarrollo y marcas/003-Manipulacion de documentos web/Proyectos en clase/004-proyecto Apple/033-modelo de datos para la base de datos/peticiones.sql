93a5b0807033c2936e22b838e1dbec04ec256e6a
Luis Rodriguez <luisrocedev@gmail.com>
2024-12-01 21:04:41 +0100

    1

---
+++ Lenguaje de desarrollo y marcas/003-Manipulacion de documentos web/Proyectos en clase/004-proyecto Apple/033-modelo de datos para la base de datos/peticiones.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `proyectoapple`.`oferta` (`Identificador` INT NOT NULL AUTO_INCREMENT , `texto` VARCHAR(255) NOT NULL , `enlace` VARCHAR(255) NOT NULL , `fehcainicio` DATE NOT NULL , `fechafinal` DATE NOT NULL , PRIMARY KEY (`Identificador`)) ENGINE = InnoDB;
