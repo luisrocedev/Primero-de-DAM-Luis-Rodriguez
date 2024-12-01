2a8ea093c311b09f9aa4c8bcad93e02f34da08ed
Luis Rodriguez <luisrocedev@gmail.com>
2024-12-01 19:46:44 +0100

    1

---
+++ Lenguaje de desarrollo y marcas/003-Manipulacion de documentos web/Proyectos en clase/004-proyecto Apple/032-template para destacados/admin/inc/cabeceras.php
@@ -0,0 +1,6 @@
+<?php
+								$columnas = ['identificador','nombre','descripcion','precio','peso'];
+								foreach ($columnas as $clave =>$valor){
+									echo "<td>".$valor."</td>";
+								}
+							?>
---
+++ Lenguaje de desarrollo y marcas/003-Manipulacion de documentos web/Proyectos en clase/004-proyecto Apple/033-modelo de datos para la base de datos/front/modulos/cabecera/cabecera.php
@@ -0,0 +1,21 @@
+
+<header>
+	<div class="contenedor">
+		<img src="img/logo.png">
+		<nav>
+			<ul>
+				<template id="elementomenu">
+					<li>
+						<a href="">hola</a>
+					</li>
+				</template>
+			</ul>
+		</nav>
+	</div>
+</header>
+<style>
+	<?php include "cabecera.css"?>
+</style>
+<script>
+	<?php include "cabecera.js"?>
+</script>
