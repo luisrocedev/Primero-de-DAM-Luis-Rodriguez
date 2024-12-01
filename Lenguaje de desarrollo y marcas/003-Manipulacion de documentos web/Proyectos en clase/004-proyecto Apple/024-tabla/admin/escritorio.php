cff3c64d66aba843b0041752c392f6e5d360c7d1
Luis Rodriguez <luisrocedev@gmail.com>
2024-12-01 15:20:07 +0100

    1

--- Lenguaje de desarrollo y marcas/003-Manipulacion de documentos web/Proyectos en clase/004-proyecto Apple/024-tabla/admin/escritorio.php
+++ Lenguaje de desarrollo y marcas/003-Manipulacion de documentos web/Proyectos en clase/004-proyecto Apple/024-tabla/admin/escritorio.php
@@ -28,6 +28,17 @@
 							?>
 						</tr>
 					</thead>
+					<tbody>
+						<?php
+							for ($i=0; $i <30 ; $i++) {
+								echo "<tr>";
+								foreach ($columnas as $clave =>$valor){
+									echo "<td>".$valor."</td>";
+								}
+								echo "</tr>";
+							}
+						?>
+					</tbody>
 				</table>
 			</section>
 		</main>
