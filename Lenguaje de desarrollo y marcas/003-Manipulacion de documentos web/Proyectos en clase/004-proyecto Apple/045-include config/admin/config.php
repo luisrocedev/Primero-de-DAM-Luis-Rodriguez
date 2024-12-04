4e6c47f60888d9dbf09dbf778965c1b719bb4e54
Luis Rodriguez <luisrocedev@gmail.com>
2024-12-04 01:35:57 +0100

    1

---
+++ Lenguaje de desarrollo y marcas/003-Manipulacion de documentos web/Proyectos en clase/004-proyecto Apple/045-include config/admin/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    $servidor = "localhost";
+	$usuario = "proyectoapple";
+	$contrasena = "proyectoapple";
+	$base = "proyectoapple";
+
+	$conexion = new mysqli($servidor, $usuario, $contrasena, $base);
+?>
