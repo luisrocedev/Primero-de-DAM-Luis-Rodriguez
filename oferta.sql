caeb315b4efff3f746846515b24617b84868293e
luisrocedev <luisilustraciones@gmail.com>
2024-12-07 09:36:45 +0100

    1

--- oferta.sql
+++ oferta.sql
@@ -33,7 +33,7 @@ CREATE TABLE `oferta` (
   `enlace` varchar(255) NOT NULL,
   `fehcainicio` date NOT NULL,
   `fechafinal` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `oferta`
