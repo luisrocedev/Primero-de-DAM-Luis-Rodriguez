ed4d540284d8544015c7b5513e8803d4eab2ca5a
luisrocedev <luisilustraciones@gmail.com>
2024-12-22 11:45:24 +0100

    1

--- Proyecto TaronjaBox/014-modular/index.php
+++ Proyecto TaronjaBox/014-modular/index.php
@@ -1,130 +1,18 @@
 <!doctype html>
 <html>
-	<head>
-		<title>Taronja Box</title>
-		<link rel="Stylesheet" href="estilo/general.css">
-		<link rel="Stylesheet" href="estilo/cabecera.css">
-		<link rel="Stylesheet" href="estilo/oferta.css">
-		<meta charset="utf-8">
-	</head>
-	<body>
-		<header>
-			<div class="contenedor">
-				<img src="logo.svg">
-				<nav>
-					<ul>
-						<li>
-							<a href="">Inicio</a>
-						</li>
-						<li>
-							<a href="">Quiénes Somos</a>
-						</li>
-						<li>
-							<a href="">Coaches</a>
-						</li>
-						<li>
-							<a href="">Horario</a>
-						</li>
-                        <li>
-							<a href="">Tarifas</a>
-						</li>
-                        <li>
-							<a href="">Contacto</a>
-						</li>
-                        <li>
-							<a href="">Localización</a>
-						</li>
-                        <li>
-							<a href="">Zona Socios</a>
-						</li>
-                        <li>
-							<a href="">Blog</a>
-						</li>
-					</ul>
-				</nav>
-			</div>
-		</header>
-        <main>
-			<section id="oferta">
-				Fíjate en esta oferta!!!!
-			</section>
-            <section id="destacado"></section>
-			<section id="heroe1">
-				<h3>Nombre del producto</h3>
-				<h4>Frase de marketing</h4>
-				<button>Call to action 1</button>
-				<button>Call to action 2</button>
-			</section>
-			<section id="heroe2">
-				<h3>Nombre del producto</h3>
-				<h4>Frase de marketing</h4>
-				<button>Call to action 1</button>
-				<button>Call to action 2</button>
-			</section>
-			<section id="heroe3">
-				<h3>Nombre del producto</h3>
-				<h4>Frase de marketing</h4>
-				<button>Call to action 1</button>
-				<button>Call to action 2</button>
-			</section>
-            <section>
-				<article>
-					<h3>Título</h3>
-					<h4>Frase de marketing</h4>
-					<button>Call to action 1</button>
-					<button>Call to action 2</button>
-				</article>
-				<article>
-					<h3>Título</h3>
-					<h4>Frase de marketing</h4>
-					<button>Call to action 1</button>
-					<button>Call to action 2</button>
-				</article>
-				<article>
-					<h3>Título</h3>
-					<h4>Frase de marketing</h4>
-					<button>Call to action 1</button>
-					<button>Call to action 2</button>
-				</article>
-				<article>
-					<h3>Título</h3>
-					<h4>Frase de marketing</h4>
-					<button>Call to action 1</button>
-					<button>Call to action 2</button>
-				</article>
-				<article>
-					<h3>Título</h3>
-					<h4>Frase de marketing</h4>
-					<button>Call to action 1</button>
-					<button>Call to action 2</button>
-				</article>
-				<article>
-					<h3>Título</h3>
-					<h4>Frase de marketing</h4>
-					<button>Call to action 1</button>
-					<button>Call to action 2</button>
-				</article>
-			</section>
-            <section>
-				<div id="carrusel1"></div>
-				<div id="botonera"></div>
-				<div id="carrusel2"></div>
-			</section>
-            <div class="contenedor">
-                Disclaimer
-            </div>
-		</main>
-        <footer>
-			<div class="contenedor">
-				<div class="columna">
-					<h3>Taronja</h3>
-					<ul>
-						<li>..</li>
-						<li>..</li>
-						<li>..</li>
-					</ul>
-				</div>
-			</div>
-		</footer>
-	</body>
+
+<head>
+	<title>Apple</title>
+	<link rel="Stylesheet" href="estilo/general.css">
+	<link rel="Stylesheet" href="estilo/cabecera.css">
+	<link rel="Stylesheet" href="estilo/oferta.css">
+	<meta charset="utf-8">
+</head>
+
+<body>
+	<?php include "modulos/cabecera/cabecera.php"; ?>
+	<?php include "modulos/principal/principal.php"; ?>
+	<?php include "modulos/piedepagina/piedepagina.php"; ?>
+</body>
+
 </html>
