3c7a9e386f24d9dba400bcec26007cc44f4a8259
Luis Rodriguez <luisrocedev@gmail.com>
2024-12-23 23:32:19 +0100

    1

---
+++ taronjabox/005-comenzamos con el js/modulos/cabecera/cabecera.js
@@ -0,0 +1,13 @@
+console.log("Soy el JS de la cabecera")
+let secciones = [
+    'Quienes somos',
+    'Esto funciona'
+]
+let cabecera = document.querySelector("header nav ul")
+secciones.forEach(function(seccion){
+    cabecera.innerHTML += `
+        <li>
+            <a href="">`+seccion+`</a>
+        </li>
+    `
+})
