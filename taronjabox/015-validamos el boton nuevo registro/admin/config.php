3cd9f759fc0aaf101a5931d0e0a841fe100f8b1b
Luis Rodriguez <luisrocedev@gmail.com>
2024-12-24 18:22:48 +0100

    1

---
+++ taronjabox/015-validamos el boton nuevo registro/admin/config.php
@@ -0,0 +1,15 @@
+<?php
+
+	$servidor = "localhost";				// Defino el servidor
+	$usuario = "taronjabox";			// Defino el usuario con permiso
+	$contrasena = "taronjabox";		// Defino la contraseña de ese usuario
+	$base = "taronjabox";				// Defino la base de datos
+
+	$conexion = new mysqli(
+		$servidor, 
+		$usuario, 
+		$contrasena, 
+		$base
+	);												// Creo una conexión de tipo MySQL
+
+?>
---
+++ taronjabox/015-validamos el boton nuevo registro/front/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Taronja Box</title>
+		<link rel="Stylesheet" href="estilo/general.css">
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<?php include "modulos/header/header.php"; ?>
+		<?php include "modulos/principal/principal.php"; ?>
+		<?php include "modulos/piedepagina/piedepagina.php"; ?>
+	</body>
+</html>
