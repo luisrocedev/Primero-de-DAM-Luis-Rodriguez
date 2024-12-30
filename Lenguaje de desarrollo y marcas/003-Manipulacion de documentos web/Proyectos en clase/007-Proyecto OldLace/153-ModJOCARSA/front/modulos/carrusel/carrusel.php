c9a5fe1ae63e76fc30903267670cd2e0ea5769a3
luisrocedev <luisilustraciones@gmail.com>
2024-12-30 23:35:30 +0100

    1

---
+++ Lenguaje de desarrollo y marcas/003-Manipulacion de documentos web/Proyectos en clase/007-Proyecto OldLace/153-ModJOCARSA/front/modulos/carrusel/carrusel.php
@@ -0,0 +1,105 @@
+<section id="carrusel">
+	<div class="contenedor">
+		<div id="carrusel1" class="animado1">
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+		</div>
+		<div id="botonera">
+			<div class="punto"></div>
+			<div class="punto"></div>
+			<div class="punto"></div>
+			<div class="punto"></div>
+			<div class="punto"></div>
+			<div class="punto"></div>
+			<div class="punto"></div>
+			<div class="punto"></div>
+		</div>
+		<div id="carrusel2" class="animado2">
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+			<article>
+				<h4>Pequeño titulo</h4>
+				<p>Pequeño texto</p>
+				<button>Call to action</button>
+			</article>
+		</div>
+	</div>
+</section>
+	
+	<script>
+	<?php include "carrusel.js"?>
+</script>
+<style>
+	<?php include "carrusel.css"?>
+</style>
