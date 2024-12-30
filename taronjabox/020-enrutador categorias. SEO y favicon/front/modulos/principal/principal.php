f29a086f8af5815ac0675d8e7ae7fa6520c554ac
luisrocedev <luisilustraciones@gmail.com>
2024-12-31 00:27:51 +0100

    1

--- taronjabox/020-enrutador categorias. SEO y favicon/front/modulos/principal/principal.php
+++
@@ -1,9 +0,0 @@
-<main>
-	
-	<?php include "modulos/oferta/oferta.php" ?>
-	<?php include "modulos/heroes/heroes.php" ?>
-	<?php include "modulos/tarifa/tarifa.php" ?>
-	<?php include "modulos/carrusel/carrusel.php" ?>
-
-	
-</main>
