cbf2944a0b67322ad4a56b3dac0ea7cf90ffd738
Luis Rodriguez <luisrocedev@gmail.com>
2025-01-10 14:06:04 +0100

    1

--- TaronjaboxNew/004-CRUD Entrenador - copia/admin/models/entrenador/ListEntrenadorModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../../../helpers/BaseModel.php';
-
-class AdminListEntrenadorModel extends BaseModel {
-    public function getAllEntrenadores() {
-        return $this->fetchAll('entrenadores');
-    }
-}
-?>
