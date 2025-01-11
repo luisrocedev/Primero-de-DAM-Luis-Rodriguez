88d7b49ec264f7d9875f84ce4d5552508b04e874
luisrocedev <luisilustraciones@gmail.com>
2025-01-11 18:45:22 +0100

    1

--- TaronjaboxNew/003-Confg mod entrenadores/config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'home' => '/front/views/home.php',
-    'entrenadores' => '/front/controllers/Entrenador/ListEntrenadorController.php',
-    'precios' => '/front/controllers/Precio/ShowPrecioController.php',
-    'blog' => '/front/controllers/Blog/ListBlogController.php',
-];
-?>
--- TaronjaboxNew/003-Confg mod entrenadores/front/models/entrenador/ListEntrenadoModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../../../helpers/BaseModel.php';
-
-class ListEntrenadorModel extends BaseModel {
-    public function getAllEntrenadores() {
-        return $this->fetchAll('entrenadores'); // Asume que la tabla se llama "entrenadores"
-    }
-}
-?>
