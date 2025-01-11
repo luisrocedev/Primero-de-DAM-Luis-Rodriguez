93e699667db1a98bdada304cea0fd48243313beb
Luis Rodriguez <luisrocedev@gmail.com>
2025-01-11 20:38:37 +0100

    1

---
+++ TaronjaboxNew modulo/016-actualizar bd /backend/controllers/PrecioController.php
@@ -0,0 +1,40 @@
+<?php
+require_once '../models/Precio.php';
+
+class PrecioController {
+    private $precio;
+
+    public function __construct() {
+        $this->precio = new Precio();
+    }
+
+    public function handleRequest() {
+        $method = $_SERVER['REQUEST_METHOD'];
+        switch ($method) {
+            case 'GET':
+                echo json_encode($this->precio->getAllPrices());
+                break;
+            case 'POST':
+                $nombre_plan = $_POST['nombre_plan'] ?? '';
+                $descripcion = $_POST['descripcion'] ?? '';
+                $precio = $_POST['precio'] ?? 0;
+                echo json_encode(['success' => $this->precio->addPrice($nombre_plan, $descripcion, $precio)]);
+                break;
+
+            case 'PUT': // Editar un precio existente
+                parse_str(file_get_contents("php://input"), $_PUT);
+                $id = $_PUT['id'] ?? 0;
+                $nombre_plan = $_PUT['nombre_plan'] ?? '';
+                $descripcion = $_PUT['descripcion'] ?? '';
+                $precio = $_PUT['precio'] ?? 0.0;
+                echo json_encode(['success' => $this->precio->updatePrice($id, $nombre_plan, $descripcion, $precio)]);
+                break;
+                
+            case 'DELETE':
+                $id = $_GET['id'] ?? 0;
+                echo json_encode(['success' => $this->precio->deletePrice($id)]);
+                break;
+        }
+    }
+}
+?>
