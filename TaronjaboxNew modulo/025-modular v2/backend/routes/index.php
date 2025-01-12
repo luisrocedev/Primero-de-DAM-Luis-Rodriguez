a6d30b44e0444aa0653deae4273c9a32fef6bff6
luisrocedev <luisilustraciones@gmail.com>
2025-01-13 00:34:22 +0100

    1

---
+++ TaronjaboxNew modulo/025-modular v2/backend/routes/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'contacto.php';
+require_once 'entrenadores.php';
+require_once 'horarios.php';
+require_once 'blog.php';
+require_once 'sobre_nosotros.php';
+require_once 'precios.php';
+?>
--- TaronjaboxNew modulo/025-organizacion/backend/routes/sobre_nosotros.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once '/backend/routes/entrenadores.php';
-
-$controller = new SobreNosotrosController();
-$controller->handleRequest();
-?>
