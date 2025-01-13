5637cea2c4a9055912fe3a87df7e55cec3204977
luisrocedev <luisilustraciones@gmail.com>
2025-01-13 15:45:04 +0100

    1

--- TaronjaboxNew modulo/026-constants/helpers/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('BASE_URL', 'http://localhost/025-organizacion');
-define('ADMIN_URL', BASE_URL . '/admin');
-define('FRONT_URL', BASE_URL . '/front');
-define('BACKEND_URL', BASE_URL . '/backend');
-define('UPLOADS_URL', BASE_URL . '/uploads');
-?>
