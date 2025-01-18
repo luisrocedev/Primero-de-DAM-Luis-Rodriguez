0da79494b2a0f0597fbe0b879030ab715d24f9a6
luisrocedev <luisilustraciones@gmail.com>
2025-01-19 00:39:20 +0100

    1

--- taronjabox_valencia/012-Documentacion(en proceso)/backend/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'taronjaboxvalencia');
-define('DB_PASSWORD', 'taronjaboxvalencia');
-define('DB_NAME', 'taronjaboxvalencia');
-define('BASE_URL', '/Primero-de-DAM-Luis-Rodriguez/taronjabox_valencia/');
