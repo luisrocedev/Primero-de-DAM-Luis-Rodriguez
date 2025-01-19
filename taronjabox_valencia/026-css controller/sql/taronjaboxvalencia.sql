42975b55b4b98ce716c9278c9be2fa03a257dde9
luisrocedev <luisilustraciones@gmail.com>
2025-01-19 16:22:44 +0100

    1

--- taronjabox_valencia/026-css controller/sql/taronjaboxvalencia.sql
+++ taronjabox_valencia/026-css controller/sql/taronjaboxvalencia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-01-2025 a las 19:16:52
+-- Tiempo de generación: 19-01-2025 a las 16:22:22
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -36,6 +36,13 @@ CREATE TABLE `clases` (
   `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `clases`
+--
+
+INSERT INTO `clases` (`id`, `nombre`, `descripcion`, `horario`, `capacidad`, `fecha_creacion`) VALUES
+(3, 'fsd', 'fsd', 'fsd', 12, '2025-01-19 12:48:35');
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +56,13 @@ CREATE TABLE `galeria` (
   `fecha_subida` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `galeria`
+--
+
+INSERT INTO `galeria` (`id`, `titulo`, `imagen`, `fecha_subida`) VALUES
+(4, 'esto es una prueba sd', '1280369.png', '2025-01-18 19:43:26');
+
 -- --------------------------------------------------------
 
 --
@@ -60,8 +74,16 @@ CREATE TABLE `mensajes_contacto` (
   `nombre` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `mensaje` text NOT NULL,
-  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp(),
+  `leido` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `mensajes_contacto`
+--
+
+INSERT INTO `mensajes_contacto` (`id`, `nombre`, `email`, `mensaje`, `fecha`, `leido`) VALUES
+(3, 'prueba', 'luis@gmail.com', 'texto prueba', '2025-01-19 13:07:12', 1);
 
 -- --------------------------------------------------------
 
@@ -90,6 +112,14 @@ CREATE TABLE `usuarios_admin` (
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios_admin`
+--
+
+INSERT INTO `usuarios_admin` (`id`, `username`, `password`, `created_at`) VALUES
+(1, 'admin', 'admin', '2025-01-19 00:42:27'),
+(2, 'nuevo_usuario', 'contraseña123', '2025-01-19 01:00:52');
 
 --
 -- Índices para tablas volcadas
@@ -123,7 +153,8 @@ ALTER TABLE `noticias_eventos`
 -- Indices de la tabla `usuarios_admin`
 --
 ALTER TABLE `usuarios_admin`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -133,31 +164,31 @@ ALTER TABLE `usuarios_admin`
 -- AUTO_INCREMENT de la tabla `clases`
 --
 ALTER TABLE `clases`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `galeria`
 --
 ALTER TABLE `galeria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `mensajes_contacto`
 --
 ALTER TABLE `mensajes_contacto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `noticias_eventos`
 --
 ALTER TABLE `noticias_eventos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios_admin`
 --
 ALTER TABLE `usuarios_admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
