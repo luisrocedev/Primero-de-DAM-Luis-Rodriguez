d3a7af9b1377631baeb87ec51c405aa47ec603f2
Luis Rodriguez <luisrocedev@gmail.com>
2025-01-23 14:33:47 +0100

    1

--- taronja_web/013-mas secciones admin/backend/header.php
+++
@@ -1,9 +0,0 @@
-// Ejemplo básico de fetch para obtener datos del backend
-fetch('backend.php')
-    .then(response => response.json())
-    .then(data => {
-        console.log('Datos recibidos:', data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
