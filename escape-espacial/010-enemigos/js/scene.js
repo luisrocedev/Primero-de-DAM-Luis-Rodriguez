ba8c4c3c2b70a5feebcf777dd6fde1e3586bca00
luisrocedev <luisilustraciones@gmail.com>
2025-02-01 13:50:53 +0100

    1

--- escape-espacial/010-enemigos/js/scene.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("assets/scene.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("scene-container").innerHTML = data;
-        })
-        .catch(error => console.error("Error cargando la escena:", error));
-});
